f37a4621b7c1eb5001b1b8a2ddba9e87d18275c8
Goutam paswan <pritampaswan06@gmail.com>
2026-06-28 14:57:41 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
 
+num1 = float(input("Enter first number :"))
+op = input("choose operator  (+,-,*,/ ) :")
+num2 = float(input("Enter second number : "))
+
+if op == "+" :
+	 print("result = ", num1 + num2)
+	 
+elif op == "-" :
+	print("Result = " , num1 - num2)
+elif op == "*" :
+	print("Result = " , num1 * num2)
+elif op == "/" :
+	print("Result = " , num1 / num2)
+	
+else:
+	print("Invalid opreation")
