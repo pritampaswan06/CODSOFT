fcba3f8d52357b86c321532a594d9f9669d5bacd
Goutam paswan <pritampaswan06@gmail.com>
2026-06-28 15:04:10 +0530

    password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+length = int(input("Password length: "))
+
+chars = string.ascii_letters + string.digits + string.punctuation
+
+password = ''.join(random.choice(chars) for i in range(length))
+
+print("Generated Password:", password)
